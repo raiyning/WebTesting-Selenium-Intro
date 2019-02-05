ee09d31be32654bc91270146651c5c307958edcb
Raiyan Shaheen <r.12345@hotmail.co.uk>
2019-02-05 14:17:42 +0000

    structure change; library,generator and gem file added

--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -6,11 +6,23 @@ describe 'testing the demo qatesting auto form' do
     @driver = QatoolsForm.new
     @driver.visit_practice_form
     @url = 'https://www.toolsqa.com/automation-practice-form'
+    @name = Faker::HarryPotter.character
+    @lotr = Faker::LordOfTheRings.character
+    @place = Faker::LordOfTheRings.location
   end
   context 'testing the positive paths for the form' do
 
     it 'should land on regi demo form page' do
       expect(@driver.current_url).to eq @url
+    end
+
+    it 'should accept a first name ' do
+      @driver.input_firstname_field(@name)
+      expect(@driver.input_firstname_field_value).to eq @name
+    end
+
+    it 'dasd' do
+      p "oh no president#{Faker::HarryPotter.character} get down hes gonna do it word, Then #{Faker::HarryPotter.character} looked into #{Faker::LordOfTheRings.character} into his eyes. The passion was warmer than #{Faker::LordOfTheRings.location}. it was forbidden, but they couldnt help it. Then they finished eating dinner and went home as platoninc friends"
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'selenium-webdriver'
+require 'faker'
 require_relative '../selenium_class_walkthrough'
+
 
 RSpec.configure do |config|
   config.formatter = :documentation
